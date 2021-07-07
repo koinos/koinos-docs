a1b2ed055666da1101dc7a593642a7d6a1014778
Ron Hamenahem <ron@koinos.group>
2021-07-07 18:01:21 -0400

    add eval statement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 # add travis ssh key 
+eval `ssh-agent -s`
 ssh-add - <<< "${DEPLOY_SSH_KEY}"
 
 # Remove .gitignore and replace with the production version
@@ -7,5 +8,5 @@ cp scripts/prodignore .gitignore
 cat .gitignore
 
 # copy files inside the generated HTML directory to the webserver
-echo rsync -azPv ./docs/_build/html koinos@173.255.232.131:/var/www/html
+rsync -azPv ./docs/_build/html koinos@173.255.232.131:/var/www/html
 

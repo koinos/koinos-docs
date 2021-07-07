15efebd9415026b79c90094958eee8a8d51a3282
Ron Hamenahem <ron@koinos.group>
2021-07-07 16:26:39 -0400

    fix travis problem 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 # add travis ssh key 
-# ssh-add - <<< "${DEPLOY_SSH_KEY}"
-ssh-add -L
+ssh-add - <<< "${DEPLOY_SSH_KEY}"
 
 # Remove .gitignore and replace with the production version
 rm -f .gitignore
@@ -9,5 +8,6 @@ cat .gitignore
 
 # copy files inside the generated HTML directory to the webserver
 ssh-keyscan hostname > known_hosts
+cat ~/.ssh/id_rsa.pub
 rsync -e "ssh -o StrictHostKeyChecking=no" -azPv ./docs/_build/html koinos@173.255.232.131:/var/www/html
 

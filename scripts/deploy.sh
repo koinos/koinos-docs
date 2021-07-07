f34f72bccce245c88e66fe0b192faebff9676528
Ron Hamenahem <ron@koinos.group>
2021-07-07 13:22:11 -0400

    fix travis problem 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,9 @@ rm -f .gitignore
 cp scripts/prodignore .gitignore
 cat .gitignore
 
+
+
 # copy files inside the generated HTML directory to the webserver
+ssh-keyscan hostname > known_hosts
 rsync -e "ssh -o StrictHostKeyChecking=no" -azP ./docs/_build/html koinos@173.255.232.131:/var/www/html
 

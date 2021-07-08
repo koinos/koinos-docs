6b070d8d192c3cbfd49217746a0e87f777aefd5c
Ron Hamenahem <ron@koinos.group>
2021-07-07 21:21:41 -0400

    try to ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 # add travis ssh key 
 eval `ssh-agent -s`
 ssh-add - <<< "${DEPLOY_SSH_KEY}"
-cat /home/travis/.ssh/tmate.pub
+ssh koinos@173.255.232.131
 
 # Remove .gitignore and replace with the production version
 rm -f .gitignore

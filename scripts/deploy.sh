461f9b8ffe398b769d3cd66ffa272a4d6344dad9
Ron Hamenahem <ron@koinos.group>
2021-07-07 20:59:14 -0400

    make repo private

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 # add travis ssh key 
 eval `ssh-agent -s`
 ssh-add - <<< "${DEPLOY_SSH_KEY}"
+ls -la .ssh
 
 # Remove .gitignore and replace with the production version
 rm -f .gitignore
@@ -8,5 +9,4 @@ cp scripts/prodignore .gitignore
 cat .gitignore
 
 # copy files inside the generated HTML directory to the webserver
-rsync -azPv ./docs/_build/html koinos@173.255.232.131:/var/www/html
-
+rsync -azP ./docs/_build/html koinos@173.255.232.131:/var/www/html

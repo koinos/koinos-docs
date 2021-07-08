4e37aa14dbb33f06b101e57145b24b250e364da4
Ron Hamenahem <ron@koinos.group>
2021-07-07 21:31:47 -0400

    list .ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 # add travis ssh key 
 eval `ssh-agent -s`
 ssh-add - <<< "${DEPLOY_SSH_KEY}"
-ssh koinos@173.255.232.131
+ls -la /home/travis/.ssh
 
 # Remove .gitignore and replace with the production version
 rm -f .gitignore

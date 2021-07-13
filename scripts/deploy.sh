e10a9b04990e032eed9221286d18fdee6ac07728
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-07-13 10:55:48 -0700

    Cleanup deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,15 +5,12 @@ set -x
 
 # add travis ssh key
 eval `ssh-agent -s`
-#ssh-add - <<< "${DEPLOY_SSH_KEY}"
 
 
 # Remove .gitignore and replace with the production version
 rm -f .gitignore
 cp scripts/prodignore .gitignore
-cat .gitignore
 
 
 # Push all changes to the Linode production server
-# git push -f production HEAD:refs/heads/master
 rsync -azP ./docs/_build/html/ koinos@173.255.232.131:/var/www/html
